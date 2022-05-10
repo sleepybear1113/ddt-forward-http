dea0c6d4721bde03d9db9a202c0cf5c47ab7f747
谢家星 <569750408@qq.com>
2022-05-10 09:37:56 +0800

    @init

---
+++ src/main/java/cn/xiejx/ddtforwardhttp/DdtForwardHttpApplication.java
@@ -0,0 +1,13 @@
+package cn.xiejx.ddtforwardhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdtForwardHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdtForwardHttpApplication.class, args);
+    }
+
+}
